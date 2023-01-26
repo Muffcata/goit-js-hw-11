0bf1c62ddc9e4c057c81821d179b001397288dc7
Muffcata <marta.oched@gmail.com>
2023-01-26 18:42:00 +0100

    up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
-const API_KEY = '33110181-05f7c31bd0648b87ed812e30c';
+// import { fetchGallery } from './fetchGallery';
 
 const form = document.querySelector('.search-form');
 const input = document.querySelector('input[name="searchQuery"]');
+const API_KEY = '33110181-05f7c31bd0648b87ed812e30c';
 
 fetch(
-  `https://pixabay.com/api/?key='33110181-05f7c31bd0648b87ed812e30c'&q=flower`
+  `https://pixabay.com/api/?key=${API_KEY}&q={newName}&image_type=photo&orientation=horizontal&safesearch=true`
 )
   .then(r => r.json())
-  .then(r => console.log(r))
+
   .catch(e => console.log('error', e));
