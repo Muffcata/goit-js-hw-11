19d024096af49e4082425c113b708ac9c89593be
Muffcata <107173936+Muffcata@users.noreply.github.com>
2023-01-26 00:07:50 +0000

    Deploying to gh-pages from @ Muffcata/goit-js-hw-11@7fcb67a061401812c4eca03d422ccf1e96f6690c 🚀

---
+++ index.fcecf84c.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form"),document.querySelector('input[name="searchQuery"]');fetch("https://pixabay.com/api/?key='33110181-05f7c31bd0648b87ed812e30c'&q=flower").then((e=>e.json())).then((e=>console.log(e))).catch((e=>console.log("error",e)));
+//# sourceMappingURL=index.fcecf84c.js.map
