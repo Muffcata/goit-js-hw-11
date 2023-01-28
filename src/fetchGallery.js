0887b97f8bb79d49b98d3a6dcbca4afc6a09fdfe
Muffcata <marta.oched@gmail.com>
2023-01-28 16:34:50 +0100

    Update fetchGallery.js

--- src/fetchGallery.js
+++ src/fetchGallery.js
@@ -1,12 +1,19 @@
 const API_KEY = '33110181-05f7c31bd0648b87ed812e30c';
 
-export function fetchGallery(name) {
-  return fetch(
+// export function fetchGallery(name) {
+//   return fetch(
+//     `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+
+export async function fetchGallery(name) {
+  const name = await fetch(
     `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  );
+  return name.json();
 }
